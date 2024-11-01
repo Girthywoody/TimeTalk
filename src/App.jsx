9e6087c704f766978325b518aede822c2b4115cc
Joshua Law <joshua-law@hotmail.com>
2024-10-31 20:26:22 -0400

    Simplify auth flow

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 import { Loader2 } from 'lucide-react';
 import { useAuth } from './hooks/useAuth';
 import WelcomePage from './components/WelcomePage';
-import SettingsPage from './profile/SettingsPage';
+import SettingsPage from './components/profile/SettingsPage';  // if SettingsPage is in components/profile
 
 
 const App = () => {
