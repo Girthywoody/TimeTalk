25ae5f2fbc562becc8020d7099484bdb36a99053
Joshua Law <joshua-law@hotmail.com>
2024-11-07 18:38:23 -0500

    Simplify auth flow

--- src/hooks/useNotifications.js
+++ src/hooks/useNotifications.js
@@ -49,9 +49,25 @@ export const useNotifications = () => {
                 updateViaCache: 'none'
             });
 
+            function urlBase64ToUint8Array(base64String) {
+                const padding = '='.repeat((4 - base64String.length % 4) % 4);
+                const base64 = (base64String + padding)
+                    .replace(/\-/g, '+')
+                    .replace(/_/g, '/');
+
+                const rawData = window.atob(base64);
+                const outputArray = new Uint8Array(rawData.length);
+
+                for (let i = 0; i < rawData.length; ++i) {
+                    outputArray[i] = rawData.charCodeAt(i);
+                }
+                return outputArray;
+            }
+
             // Get FCM token
+            const vapidKey = 'BJ9j4bdUtNCIQtWDls0PqGtSoGW__yJSv4JZSOXzkuKTizgWLsmYC1t4OxiYx4lrpbcNGm1IUobk_8dGLwvycc';
             const token = await getToken(messaging, {
-                vapidKey: 'BJ9j4bdUtNCIQtWDls0PqGtSoGW__yJSv4JZSOXzkuKTizgWLsmYC1t4OxiYx4lrpbcNGm1IUobk_8dGLwvycc',
+                vapidKey: vapidKey,
                 serviceWorkerRegistration: registration
             });
 
