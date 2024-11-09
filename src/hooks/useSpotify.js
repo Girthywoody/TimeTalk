8df717691544fa7147f3c4fc9ed0ad0967aee4f9
Joshua Law <joshua-law@hotmail.com>
2024-11-09 17:23:06 -0500

    Simplify auth flow

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -14,7 +14,7 @@ export const useSpotify = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': `Basic ${btoa(`${process.env.REACT_APP_SPOTIFY_CLIENT_ID}:${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`)}`,
+          'Authorization': `Basic ${btoa(`${import.meta.env.VITE_SPOTIFY_CLIENT_ID}:${import.meta.env.VITE_SPOTIFY_CLIENT_SECRET}`)}`,
         },
         body: 'grant_type=client_credentials'
       });
